24665574844cd553ee532af91a32fecbeafa051d
MarcioAciole <aciolester@gmail.com>
2025-07-21 19:05:14 -0300

    Add files via upload

--- src app page.tsx
+++ src app page.tsx
@@ -1,15 +1,3 @@
-import Hero from "@/components/sections/Hero";
-import Services from "@/components/sections/Services";
-import LeadCapture from "@/components/sections/LeadCapture";
-import Testimonials from "@/components/sections/Testimonials";
-
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Services />
-      <LeadCapture />
-      <Testimonials />
-    </>
-  );
+  return <h1>Hello, Property Reg Expert!</h1>;
 }
